f61976b752127c2e991e5dd0b594210ff9c80bd4
Balazs Gabor <v-bgabor@hotels.com>
2020-01-20 10:37:18 +0100

    Docker and tests are switchable

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,26 +2,35 @@
 #
 # Starter script for Smart Platform
 
+UPDATE=false
+TESTS=false
+DOCKER=false
+
 COLOR_HEADER="\033[7;49;36m"
 COLOR_RESET="\033[0m"
 
+# 0 - project name
+# 1 - git repository
+# 2 - service port
 LOGGING_FILTER="smart-logging-filter;https://github.com/ProudProgrammer/smart-logging-filter.git"
 LOTTERY_SERVICE_CLIENT="smart-lottery-service-client;https://github.com/ProudProgrammer/smart-lottery-service-client.git"
-LOTTERY_SERVICE="smart-lottery-service;https://github.com/ProudProgrammer/smart-lottery-service.git;-Pdocker,fast;8081"
-EDGE_SERVICE="smart-edge-service;https://github.com/ProudProgrammer/smart-edge-service.git;-Pdocker,fast;8080"
+LOTTERY_SERVICE="smart-lottery-service;https://github.com/ProudProgrammer/smart-lottery-service.git;8081"
+EDGE_SERVICE="smart-edge-service;https://github.com/ProudProgrammer/smart-edge-service.git;8080"
 
 PROJECTS=("$LOGGING_FILTER" "$LOTTERY_SERVICE_CLIENT" "$LOTTERY_SERVICE" "$EDGE_SERVICE")
 
-UPDATE=false
-
 cd ../../
 MAIN_DIR=$(pwd)
+
+MAVEN_PROFILES_ACTIVE=""
 
 function print_help() {
     echo "Usage: ./start.sh [-u | --update]"
     echo "Options:"
     echo "    -h, --h         print help"
     echo "    -u, --update    update git repositories"
+    echo "    -t, --tests     run unit and integration tests"
+    echo "    -d, --docker    build images, clean containers and images, start containers"
     echo ""
     echo "Automation script for Smart-Platform."
     echo "It can clone/pull git repositories, build jars, build docker images, start containers, delete old docker images and containers."
@@ -32,6 +41,8 @@ function process_args() {
         case "${1}" in
           -h | --help) print_help && exit 0 ;;
           -u | --update) UPDATE=true ;;
+          -t | --tests) TESTS=true ;;
+          -d | --docker) DOCKER=true ;;
         esac
         shift
     done
@@ -41,8 +52,10 @@ function check_dependencies(){
     echo -e "\n${COLOR_HEADER}Checking dependencies...${COLOR_RESET}"
     command -v git >/dev/null 2>&1 || { echo >&2 "Git is not installed. Exiting..."; exit -1; }
     command -v mvn >/dev/null 2>&1 || { echo >&2 "Maven is not installed. Exiting..."; exit -1; }
-    command -v jq >/dev/null 2>&1 || { echo >&2 "Jq is not installed. Exiting..."; exit -1; }
-    command -v docker >/dev/null 2>&1 || { echo >&2 "Docker is not installed. Exiting..."; exit -1; }
+    if [[ ${DOCKER} == true ]]; then
+        command -v jq >/dev/null 2>&1 || { echo >&2 "Jq is not installed. Exiting..."; exit -1; }
+        command -v docker >/dev/null 2>&1 || { echo >&2 "Docker is not installed. Exiting..."; exit -1; }
+    fi
 }
 
 function clone_repositories() {
@@ -66,6 +79,16 @@ function clone_repositories() {
     IFS=' '
 }
 
+function set_maven_profiles_active() {
+    if [[ ${TESTS} == false && ${DOCKER} == false ]]; then
+        MAVEN_PROFILES_ACTIVE="-Pfast"
+    elif [[ ${TESTS} == false && ${DOCKER} == true  ]]; then
+        MAVEN_PROFILES_ACTIVE="-Pfast,docker"
+    elif [[ ${TESTS} == true && ${DOCKER} == true ]]; then
+        MAVEN_PROFILES_ACTIVE="-Pdocker"
+    fi
+}
+
 function build_projects() {
     IFS=';'
     for PROJECT in "${PROJECTS[@]}"
@@ -73,7 +96,7 @@ function build_projects() {
         read -ra PROJECT_AS_ARRAY <<< "$PROJECT"
         echo -e "\n${COLOR_HEADER}Building ${PROJECT_AS_ARRAY[0]}...${COLOR_RESET}"
         cd "$MAIN_DIR/${PROJECT_AS_ARRAY[0]}" || exit
-        mvn clean install ${PROJECT_AS_ARRAY[2]}
+        mvn clean install ${MAVEN_PROFILES_ACTIVE}
     done
     IFS=' '
 }
@@ -87,8 +110,8 @@ function clean_containers_and_images() {
 
 function start_containers() {
     echo -e "\n${COLOR_HEADER}Starting containers...${COLOR_RESET}"
-    LOTTERY_SERVICE_PORT=$(echo "${LOTTERY_SERVICE}" | cut -d ';' -f 4)
-    EDGE_SERVICE_PORT=$(echo "${EDGE_SERVICE}" | cut -d ';' -f 4)
+    LOTTERY_SERVICE_PORT=$(echo "${LOTTERY_SERVICE}" | cut -d ';' -f 3)
+    EDGE_SERVICE_PORT=$(echo "${EDGE_SERVICE}" | cut -d ';' -f 3)
     GATEWAY=$(docker network inspect bridge | jq -r '.[0].IPAM.Config | .[0].Gateway')
     docker run -d -p ${LOTTERY_SERVICE_PORT}:${LOTTERY_SERVICE_PORT} lottery-service:1.0-SNAPSHOT
     docker run -d -p ${EDGE_SERVICE_PORT}:${EDGE_SERVICE_PORT} -e lottery.service.base.url=${GATEWAY}:${LOTTERY_SERVICE_PORT} edge-service:1.0-SNAPSHOT
@@ -98,9 +121,12 @@ function init() {
     START_TIME=$SECONDS
     check_dependencies
     clone_repositories
+    set_maven_profiles_active
     build_projects
-    clean_containers_and_images
-    start_containers
+    if [[ ${DOCKER} == true ]]; then
+        clean_containers_and_images
+        start_containers
+    fi
     ELAPSED_TIME=$(($SECONDS - $START_TIME))
     ((SEC=ELAPSED_TIME%60, ELAPSED_TIME/=60, MIN=ELAPSED_TIME%60, HRS=ELAPSED_TIME/60))
     TIMESTAMP=$(printf "%02d:%02d:%02d" ${HRS} ${MIN} ${SEC})

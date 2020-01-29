db3effa3eddedeaf05120d84ff54e0870de43ae0
Balazs Gabor <v-bgabor@hotels.com>
2020-01-29 10:50:27 +0100

    Stopper script

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # Smart Platform - Tools
-
 Tools for Smart Platform
-
 ### Tools
 ```
 - JMeter JMX test plans as performance tests
@@ -9,7 +7,6 @@ Tools for Smart Platform
 - Postman collections for manual testing
 - Shell scripts for automation of builds, run, etc.
 ```
-
 ### Starter script
 ```
 Usage: ./start.sh [-u | --update -t | --tests -d | --docker]
@@ -22,7 +19,15 @@ Options:
 Automation script for Smart-Platform.
 It can clone/pull git repositories, build jars, build docker images, start containers, delete old docker images and containers.
 ```
+### Stopper script
+```
+Usage: ./stop.sh
+Options:
+    -h, --h         print help
 
+Automation script for Smart-Platform.
+It can stop containers.
+```
 ### System architecture of Smart Platform
 Applied software development techniques:
 - Microservice Architecture
---
+++ scripts/stop.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#
+# Stopper script for Smart Platform
+
+COLOR_HEADER="\033[7;49;36m"
+COLOR_RESET="\033[0m"
+
+function print_help() {
+    echo "Usage: ./stop.sh"
+    echo "Options:"
+    echo "    -h, --h         print help"
+    echo ""
+    echo "Automation script for Smart-Platform."
+    echo "It can stop containers."
+}
+
+function process_args() {
+    while [[ ${#} -gt 0 ]]; do
+        case "${1}" in
+          -h | --help) print_help && exit 0 ;;
+        esac
+        shift
+    done
+}
+
+function check_dependencies(){
+    echo -e "\n${COLOR_HEADER}Checking dependencies...${COLOR_RESET}"
+    command -v docker >/dev/null 2>&1 || { echo >&2 "Docker is not installed. Exiting..."; exit -1; }
+}
+
+
+function stop_containers() {
+    echo -e "\n${COLOR_HEADER}Stopping docker containers...${COLOR_RESET}"
+    docker container stop $(docker ps | grep "[edge|lottery]" | awk '{ print $1 }')
+}
+
+function init() {
+    START_TIME=$SECONDS
+    check_dependencies
+    stop_containers
+    ELAPSED_TIME=$(($SECONDS - $START_TIME))
+    ((SEC=ELAPSED_TIME%60, ELAPSED_TIME/=60, MIN=ELAPSED_TIME%60, HRS=ELAPSED_TIME/60))
+    TIMESTAMP=$(printf "%02d:%02d:%02d" ${HRS} ${MIN} ${SEC})
+    echo -e "\n${COLOR_HEADER}Stopper script total time:${COLOR_RESET} ${TIMESTAMP}"
+}
+
+process_args "${@}"
+init
